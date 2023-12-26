e81bd77d337fb150b5e2a136de05660cb2c06cef
RamHusam111 <husamramoni@gmail.com>
2023-12-26 16:01:21 +0200

    Test for semester class. Test Schedule class. Some changes on other classes create demo for display Schedule.

--- src/test/java/object_orienters/StudentTest.java
+++ src/test/java/object_orienters/StudentTest.java
@@ -1,14 +1,12 @@
 package object_orienters;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StudentTest {
     Student stu1;
