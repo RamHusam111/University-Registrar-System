8381f0aff4ff029ccfc7a5cd087f3bc5e7262eac
Angela Salem <angel.salem.008@gmail.com>
2023-12-30 13:24:20 +0200

    forgot to add file

---
+++ src/main/java/object_orienters/Specialization.java
@@ -0,0 +1,38 @@
+package object_orienters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Specialization {
+    private String name;
+    private Faculty faculty;
+    private Type type;
+    private List<Teacher> teachers;
+
+    public Specialization(String name, Faculty faculty, Type type) {
+        this.teachers = new ArrayList<>();
+        this.name = name;
+        this.faculty = faculty;
+        this.type = type;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type {
+        MAJOR, MINOR;
+    }
+
+    public List<Teacher> getTeachers() {
+        return this.teachers;
+    }
+}
