0b8183807bbab57b829f874fd5e797fb24914da4
RamHusam111 <husamramoni@gmail.com>
2023-12-31 04:36:25 +0200

    finalTestFor(Semester class)

--- src/test/java/object_orienters/SemesterTest.java
+++ src/test/java/object_orienters/SemesterTest.java
@@ -3,21 +3,37 @@ package object_orienters;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
 public class SemesterTest {
-    private Semester semester;
     private LocalDate start;
     private LocalDate end;
+    private List<Semester> semesters;
+    private Semester semester;
+
 
     @Before
-    public void setUp() {
-        // Set up a sample semester
+    public void setUp() throws IOException {
+        String filePath = "src/main/resources/SemestersDates.csv";
+        semesters = Files.lines(Paths.get(filePath))
+                .skip(1) // Skipping the header row
+                .map(line -> line.split(","))
+                .map(parts -> new Semester(
+                        LocalDate.parse(parts[0].trim()),
+                        LocalDate.parse(parts[1].trim())
+                ))
+                .collect(Collectors.toList());
+        semester = semesters.get(0);
         start = LocalDate.of(2023, 9, 1);
         end = LocalDate.of(2023, 12, 31);
-        semester = new Semester(start, end);
+
     }
 
     @Test
@@ -29,7 +45,7 @@ public class SemesterTest {
     public void testCalculateWeeksBetween() {
 
         long expectedWeeks = 17;
-        assertEquals(expectedWeeks, Semester.calculateWeeksBetween(start, end));
+        assertEquals(expectedWeeks, Semester.calculateWeeksBetween(semester.getSemesterStartDate(), semester.getSemesterEndDate()));
     }
 
     @Test
