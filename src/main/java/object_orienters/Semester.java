b19cfcdab81fb1db09dd18946dd62443c69ff639
RamHusam111 <husamramoni@gmail.com>
2024-01-03 19:09:43 +0200

    Javadoc comments for: Semester, Specialization, Student

--- src/main/java/object_orienters/Semester.java
+++ src/main/java/object_orienters/Semester.java
@@ -21,6 +21,15 @@ public class Semester {
     private boolean isSpring;
     private boolean isSummer;
 
+    /**
+     * Constructs a new Semester with specified start and end dates.
+     * Initializes the semester name, calculates its length in weeks, and sets flags for
+     * whether it's a Fall, Spring, or Summer semester.
+     *
+     * @param semesterStartDate The start date of the semester.
+     * @param semesterEndDate The end date of the semester.
+     */
+
     public Semester(LocalDate semesterStartDate, LocalDate semesterEndDate) {
 
         this.semesterStartDate = semesterStartDate;
@@ -37,6 +46,15 @@ public class Semester {
 
     }
 
+    /**
+     * Registers a course for the semester, including assigning a teacher and enrolling students.
+     * Checks for room conflicts, teacher availability, and student prerequisites.
+     * Only enrolls students who meet all criteria.
+     *
+     * @param course The course to be registered.
+     * @param lStudents The list of students attempting to register for the course.
+     * @param teacher The teacher assigned to the course.
+     */
     // TODO: Test this method
     public void registerInACourse(Course course, List<Student> lStudents, Teacher teacher) {
         // Check for room conflict
@@ -91,7 +109,11 @@ public class Semester {
         // Set up the course with the teacher and add to courses list
 
     }
-
+    /**
+     * Determines the name of the semester (Fall, Spring, or Summer) based on its start date.
+     *
+     * @return The name of the semester.
+     */
     public String giveName() {
         int startMonth = semesterStartDate.getMonthValue();
         if ((startMonth >= 9 && startMonth <= 12)) {
@@ -153,11 +175,25 @@ public class Semester {
         return isSummer;
     }
 
+    /**
+     * Calculates the number of weeks between two dates(Start date and End date).
+     *
+     * @param startDate The start date.
+     * @param endDate The end date.
+     * @return The number of weeks between the two dates.
+     */
     static long calculateWeeksBetween(LocalDate startDate, LocalDate endDate) {
         // Calculate the number of weeks in the Semester
         return ChronoUnit.WEEKS.between(startDate, endDate);
     }
 
+    /**
+     * Returns a string representation of the semester.
+     * This representation includes the semester's name, start date, and end date,
+     * providing a concise summary of the semester's duration and identity.
+     *
+     * @return A formatted string containing the semester's details.
+     */
     @Override
     public String toString() {
         return "Semester: " + semesterName + "[from:" + semesterStartDate + ", to:" + semesterEndDate + "]";
