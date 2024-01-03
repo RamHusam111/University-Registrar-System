06e448be25b589258f5241149f861785f2dc646b
RamHusam111 <husamramoni@gmail.com>
2024-01-03 18:44:31 +0200

    Javadoc comments for: Course, Faculty, Teacher

--- src/main/java/object_orienters/Teacher.java
+++ src/main/java/object_orienters/Teacher.java
@@ -5,6 +5,14 @@ public class Teacher extends Person {
 
   private Specialization specialization;
 
+  /**
+   * Constructs a new Teacher with the given name and specialization.
+   * Initializes the teacher with the role TEACHER and adds the teacher to the
+   * specialization's list of teachers and the faculty's list of teachers.
+   *
+   * @param name The name of the teacher.
+   * @param specialization The area of specialization for the teacher.
+   */
   public Teacher(String name, Specialization specialization) {
     super(Role.TEACHER, name);
     this.specialization = specialization;
@@ -12,11 +20,21 @@ public class Teacher extends Person {
     specialization.getFaculty().getTeachers().add(this);
 
   }
-
+  /**
+   * Returns the specialization of the teacher.
+   *
+   * @return The teacher's area of specialization.
+   */
   public Specialization getSpecialization() {
     return specialization;
   }
 
+  /**
+   * Returns a string representation of the teacher, including their basic information
+   * from the Person class and their area of specialization.
+   *
+   * @return A formatted string with the teacher's details and specialization.
+   */
   @Override
   public String toString() {
     return super.toString() + "\nSpecialization: "
